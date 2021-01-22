8cbdefc35698c0971e7946ac5eac3ed5e9b7f90d
TonyMpro <tony.mahier.pro@gmail.com>
2021-01-22 14:21:26 +0100

    +

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -7,7 +7,10 @@ use ApiPlatform\Core\Annotation\ApiResource;
 use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
- * @ApiResource()
+ * @ApiResource(
+ *     normalizationContext={"groups"={"article:read"}},
+ *     denormalizationContext={"groups"={"article:write"}}
+ * )
  * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
  */
 class User implements UserInterface
@@ -16,32 +19,44 @@ class User implements UserInterface
      * @ORM\Id()
      * @ORM\GeneratedValue()
      * @ORM\Column(type="integer")
+     * 
+     * @Groups("article:read")
      */
     private $id;
 
     /**
      * @ORM\Column(type="string", length=180, unique=true)
+     * 
+     * @Groups({"article:read", "article:write"})
      */
     private $email;
 
     /**
      * @ORM\Column(type="json")
+     * 
+     * @Groups({"article:read", "article:write"})
      */
     private $roles = [];
 
     /**
      * @var string The hashed password
      * @ORM\Column(type="string")
+     * 
+     * @Groups({"article:read", "article:write"})
      */
     private $password;
 
     /**
      * @ORM\Column(type="string", length=255)
+     * 
+     * @Groups({"article:read", "article:write"})
      */
     private $Firstname;
 
     /**
      * @ORM\Column(type="string", length=255)
+     * 
+     * @Groups({"article:read", "article:write"})
      */
     private $Lastname;
 
